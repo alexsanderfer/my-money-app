c2e90b02c6142bae7f93395087314871ad1e9e90
alexsanderfer <afgouveia90@gmail.com>
2022-07-01 11:30:33 +0100

    Updates

--- frontend/src/common/tab/tabActions.js
+++ frontend/src/common/tab/tabActions.js
@@ -5,7 +5,11 @@ export function selectTab(tabId) {
 	};
 }
 
-export function showTab(...tabIds) {
+export function showTabs(...tabIds) {
 	const tabsToShow = {};
     tabIds.forEach(e => tabsToShow[e] = true)
+	return {
+		type: "TAB_SHOWED",
+		payload: tabsToShow,
+	}
 }
--- frontend/src/common/tab/tabReducer.js
+++ frontend/src/common/tab/tabReducer.js
@@ -1,13 +1,12 @@
-const INITIAL_STATE = {selected: ''}
+const INITIAL_STATE = {selected: '', visible: ''}
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
-    case 'SELECT_TAB':
-      return {
-        ...state,
-        selected: action.payload
-      }
-    default:
-      return state
-  }
+        case 'SELECT_TAB':
+            return {...state, selected: action.payload}
+        case 'TAB_SHOWED':
+            return {...state, visible: action.payload}
+        default:
+            return state
+    }
 }

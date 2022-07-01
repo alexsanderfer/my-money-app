9a15eff2075c05a388bbe2ca0d697f191e3a546b
alexsanderfer <afgouveia90@gmail.com>
2022-07-01 18:45:48 +0100

    erro no axios.post()

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,6 +1,6 @@
-import React, {Component} from "react";
-import {bindActionCreators} from "redux";
-import {connect} from "react-redux";
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 import ContentHeader from "../common/template/contentHeader";
 import Content from "../common/template/content";
 import Tabs from "../common/tab/tabs";
@@ -8,46 +8,48 @@ import TabsHeader from "../common/tab/tabsHeader";
 import TabsContent from "../common/tab/tabsContent";
 import TabHeader from "../common/tab/tabHeader";
 import TabContent from "../common/tab/tabContent";
-import {selectTab, showTabs} from "../common/tab/tabActions";
+import { selectTab, showTabs } from "../common/tab/tabActions";
+import { create } from "./billingCycleActions";
+import List from "./billingCycleList";
+import Form from "./billingCyleForm";
 
 class BillingCycle extends Component {
-    componentWillMount() {
-        this.props.selectTab("tabList");
-        this.props.showTabs('tabList', 'tabCreate')
-    }
-
-    render() {
-        return (
-            <div>
-                <ContentHeader title="Payments Cycles" small="Registration"/>
-                <Content>
-                    <Tabs>
-                        <TabsHeader>
-                            <TabHeader label="Lists" icon="bars" target="tabList"/>
-                            <TabHeader label="Include" icon="plus" target="tabCreate"/>
-                            <TabHeader label="Change" icon="pencil" target="tabUpdate"/>
-                            <TabHeader label="Delete" icon="trash-o" target="tabDelete"/>
-                        </TabsHeader>
-                        <TabsContent>
-                            <TabContent id="tabList">
-                                <h1>Lists</h1>
-                            </TabContent>
-                            <TabContent id="tabCreate">
-                                <h1>Create</h1>
-                            </TabContent>
-                            <TabContent id="tabUpdate">
-                                <h1>Change</h1>
-                            </TabContent>
-                            <TabContent id="tabDelete">
-                                <h1>Delete</h1>
-                            </TabContent>
-                        </TabsContent>
-                    </Tabs>
-                </Content>
-            </div>
-        );
-    }
+	componentWillMount() {
+		this.props.selectTab("tabList");
+		this.props.showTabs("tabList", "tabCreate");
+	}
+	render() {
+		return (
+			<div>
+				<ContentHeader title="Payments Cycles" small="Registration" />
+				<Content>
+					<Tabs>
+						<TabsHeader>
+							<TabHeader label="Lists" icon="bars" target="tabList" />
+							<TabHeader label="Include" icon="plus" target="tabCreate" />
+							<TabHeader label="Change" icon="pencil" target="tabUpdate" />
+							<TabHeader label="Delete" icon="trash-o" target="tabDelete" />
+						</TabsHeader>
+						<TabsContent>
+							<TabContent id="tabList">
+								<List />
+							</TabContent>
+							<TabContent id="tabCreate">
+								<Form onSubmit={this.props.create}/>
+							</TabContent>
+							<TabContent id="tabUpdate">
+								<h1>Change</h1>
+							</TabContent>
+							<TabContent id="tabDelete">
+								<h1>Delete</h1>
+							</TabContent>
+						</TabsContent>
+					</Tabs>
+				</Content>
+			</div>
+		);
+	}
 }
 
-const mapDispatchToProps = (dispatch) => bindActionCreators({selectTab, showTabs}, dispatch);
+const mapDispatchToProps = (dispatch) => bindActionCreators({ selectTab, showTabs, create }, dispatch);
 export default connect(null, mapDispatchToProps)(BillingCycle);
--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,10 +1,14 @@
-import {combineReducers} from "redux";
+import { combineReducers } from "redux";
 import DashboardReducer from "../dashboard/dashboardReducer";
 import TabReducer from "../common/tab/tabReducer";
+import BillingCycleReducer from "../billingCycle/billingCycleReducer";
+import { reducer as formReducer } from "redux-form";
 
 const rootReducer = combineReducers({
-    dashboard: DashboardReducer,
-    tab: TabReducer
-})
+	dashboard: DashboardReducer,
+	tab: TabReducer,
+	billingCycle: BillingCycleReducer,
+	formReducer: formReducer,
+});
 
-export default rootReducer
+export default rootReducer;
